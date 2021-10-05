4e39db9518daefb66dd8a30021d52855e7ff00d0
Elijah Wood <elijah.v.wood@gmail.com>
2021-10-05 09:41:11 -0400

    Added EntityFramework, References, and Using statements

--- NotWikiHow™.MVC/Controllers/AccountController.cs
+++ NotWikiHow™.MVC/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using NotWikiHow_.Data;
 using NotWikiHow_.MVC.Models;
 
 namespace NotWikiHow_.MVC.Controllers
