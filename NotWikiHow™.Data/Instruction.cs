41c7ab97c23d4531c799af9b1846d00f2d4c6abc
Elijah Wood <elijah.v.wood@gmail.com>
2021-10-05 13:45:20 -0400

    Added models to data

---
+++ NotWikiHow™.Data/Instruction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWikiHow_.Data
+{
+        // A Good Portion of this code is structured in a way that allows it to sit in a database
+        // However it isn't my intention to give this datatype a dbSet.
+
+    public class Instruction
+    {
+        public int InstructId { get; set; }
+        public int TutorId { get; set; }
+        public int Step { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int ImageId { get; set; }
+    }
+}
---
+++ NotWikiHow™.Data/Tutorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWikiHow_.Data
+{
+    public class Tutorial
+    {
+        [Key]
+        public int TutorId { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Instruction> Instructions { get; set; }
+        public bool Published { get; set; } = false;
+        public DateTimeOffset CreatedUTC { get; set; }
+        public DateTimeOffset ModifiedUTC { get; set; }
+    }
+}
