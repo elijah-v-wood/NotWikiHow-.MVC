d9fbbf7bb6c518999f369ffe9eeacab3d9c4cf92
Elijah Wood <elijah.v.wood@gmail.com>
2021-10-21 02:46:09 -0400

    Added Instruction Create Model

--- NotWikiHow™.Data/Instruction.cs
+++ NotWikiHow™.Data/Instruction.cs
@@ -14,11 +14,10 @@ namespace NotWikiHow_.Data
     {
         [Key]
         public int InstructId { get; set; }
+        public int TutorId { get; set; }
         public int Step { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
 
-        public Instruction() { }
-        public Instruction(int count) { }
     }
 }
---
+++ NotWikiHow™.Models/InstructionCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWikiHow_.Models
+{
+    public class InstructionCreate
+    {
+        public int Step { get; set; }
+        public int TutorId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
