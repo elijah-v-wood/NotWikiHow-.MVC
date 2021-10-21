7624cede8722c78c01305e714e7a9981f527a77c
Elijah Wood <elijah.v.wood@gmail.com>
2021-10-21 02:59:38 -0400

    Added Instruction Create in services

---
+++ NotWikiHow™.Services/InstructionService.cs
@@ -0,0 +1,39 @@
+﻿using NotWikiHow_.Data;
+using NotWikiHow_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWikiHow_.Services
+{
+    public class InstructionService
+    {
+        private readonly Guid _userId;
+
+        public InstructionService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateInstruction(InstructionCreate model)
+        {
+            var ety = new Instruction()
+            {
+                TutorId=model.TutorId,
+                Title = model.Title,
+                Description = model.Description,
+                CreatedUTC = DateTimeOffset.Now
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+               var count = ctx.Instructions.Where(e => e.TutorId == ety.TutorId).Count();
+                ety.Step = count++;
+
+                ctx.Instructions.Add(ety);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
