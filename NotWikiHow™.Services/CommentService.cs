9e77765469e6aec43d71582722a5c6ac1cfadd69
Elijah Wood <elijah.v.wood@gmail.com>
2021-10-21 09:44:40 -0400

    added comment service

---
+++ NotWikiHow™.Services/CommentService.cs
@@ -0,0 +1,35 @@
+﻿using NotWikiHow_.Data;
+using NotWikiHow_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotWikiHow_.Services
+{
+    public class CommentService
+    {
+        private readonly Guid _userId;
+        public CommentService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateComment(CommentCreate model)
+        {
+            var ety = new Comment()
+            {
+                UserId = _userId,
+                Question = model.Question,
+                TutorId = model.TutorId,
+                CreatedUTC = DateTimeOffset.Now
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Comments.Add(ety);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
